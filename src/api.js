52c4d975663381469e1cb053d7d0565469d6078a
Sachin B N <125150833+sachin2k01@users.noreply.github.com>
2026-06-29 23:28:56 +0530

    Remove sensitive API keys from api.js
    Removed sensitive API keys from geoApiOptions and WEATHER_API_KEY.

--- src/api.js
+++ src/api.js
@@ -5,8 +5,8 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "df9ecaa746msh12d58d9baaf31e6p1b72bbjsn885d0d1d2b76",
-    "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+    "x-rapidapi-key": "",
+    "x-rapidapi-host": "",
     "Content-Type": "application/json",
   },
 };
@@ -23,4 +23,4 @@ export const geoApiOptions = {
 // https://api.openweathermap.org/data/4.0/onecall/current?lat={lat}&lon={lon}&appid={API key}
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "427933294d56ea9ccabc71dcb9483f0b";
+export const WEATHER_API_KEY = "";
